d9383f7165088505fd6ce0abf328c58cf2d61532
usmancynosure <104001924+usmancynosure@users.noreply.github.com>
2024-01-25 22:01:13 +0000

    added job position

--- app.py
+++ app.py
@@ -3,9 +3,36 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 
 
+Jobs=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Bengaluru, India',
+    'salary':'Rs. 10,00,000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Delhi, India',
+    'salary':'Rs. 15,00,000'
+  },
+  {
+    'id':3,
+    'title':'Frontend Engineer',
+    'location':'Remote',
+    'salary':'Rs. 12,00,000'
+  },
+  {
+    'id':4,
+    'title':'Backend Engineer',
+    'location':'San Francisco, USA',
+
+  }
+]
+
 @app.route("/")
 def hello():
-  return render_template("home.html")
+  return render_template("home.html",jobs=Jobs  )
 
 
 if __name__ == "__main__":
